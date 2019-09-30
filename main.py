17073f0e1747882fcff951fbaf784eeaa0471244
Mathias Hanna <mathias.hanna3@gmail.com>
2019-09-30 15:13:15 +0200

    Base de la communication entre client et serveur

---
+++ main.py
@@ -0,0 +1,33 @@
+import cmd, sys, socket
+
+#Instance principale de l'application de test
+class Main(cmd.Cmd):
+    intro = 'Xeyrus Test V0.1\n'
+    prompt = '>> '
+
+    #Démarrage de la connexion au serveur
+    def do_start(self, args):
+        #Définition de la socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Connexion
+        s.connect(("", 1111))
+        print("Connecté au serveur")
+
+        #Envoi des messages au serveur
+        message = ""
+        #Boucle pour garder le prompt
+        while message != "exit":
+            #Saisie du message
+            message = input(">> ")
+            #Envoi du message
+            s.send(message.encode())
+        #Si on saisi exit, on sort de la boucle et on se déconnecte donc du serveur
+        s.close()
+        print("Déconnecté du serveur")
+
+    def do_exit(self, args):
+        sys.exit(0)
+
+if __name__ == '__main__':
+    #Instanciation du prompt principal
+    Main().cmdloop()
